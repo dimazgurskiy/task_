fb17c4f45df45008a94f8d25a19524b6b69a67d2
name <chmutkola@gmail.com>
2018-02-18 22:47:02 +0200

    structures-3

---
+++ Струкури/3.js
@@ -0,0 +1,15 @@
+function tablet( index, num , hash , gap ) {
+
+       for ( let i = 0; i <= index; i++ ){
+           let out_put = '',
+               hash_ = i % 2  === 0 ? hash : gap,
+               gap_ = i % 2 === 0 ? gap: hash;
+           
+                    for ( let j = 0; j < num; j++ ){
+                        out_put += j % 2 === 0 ? hash_ : gap_;
+                    }
+          console.log( out_put )
+       }
+}
+
+tablet( 8 , 8 , '#' , ' ' );
